bf2ec690c849b57a8731669862a1b1824950662d
Eric Zhang <ezhang@shapeup.com>
2016-06-25 15:11:01 -0400

    Adding structure

--- site/src/app.js
+++ site/src/app.js
@@ -1,8 +1,10 @@
 export class App {
   configureRouter(config, router) {
-    config.title = 'Aurelia';
+    config.title = 'ewic';
     config.map([
-      { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' }
+      { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
+      { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Users' },
+      { route: 'about', name: 'about', moduleId: './about', nav: true, title: 'About Me' },
     ]);
 
     this.router = router;
