e3f33bc42ac26f8be5e1c6062724f889e94c9b7c
Eric Zhang <ezhang@shapeup.com>
2016-07-19 15:38:16 -0400

    commiting because

--- site/src/app.js
+++ site/src/app.js
@@ -4,6 +4,7 @@ export class App {
     config.map([
       { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
       { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Users' },
+      { route: 'gallery', name: 'gallery', moduleId: './gallery', nav: true, title: 'Gallery' },
       { route: 'about', name: 'about', moduleId: './about', nav: true, title: 'About Me' },
     ]);
 
---
+++ site/src/gallery.js
@@ -0,0 +1,5 @@
+export class Gallery {
+	const fs = require('fs');
+
+	
+}
