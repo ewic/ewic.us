8557036bb2f90efce152f9eaa403b63a94392e48
Eric Zhang <ezhang@shapeup.com>
2016-07-21 12:17:43 -0400

    Some changes

--- site/src/app.js
+++ site/src/app.js
@@ -3,8 +3,6 @@ export class App {
     config.title = 'ewic';
     config.map([
       { route: ['','welcome'], name: 'welcome', moduleId: './welcome', nav: true, title:'Welcome' },
-      { route: 'users', name: 'users', moduleId: './users', nav: true, title: 'Users' },
-      { route: 'gallery', name: 'gallery', moduleId: './gallery', nav: true, title: 'Gallery' },
       { route: 'about', name: 'about', moduleId: './about', nav: true, title: 'About Me' },
     ]);
 
